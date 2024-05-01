b7e40bdf79c0f7fe45faba8b1bdd1ce0a3f907dc
Facu <facundolopez026@gmail.com>
2024-05-01 20:00:38 -0300

    Armando la API con un data json

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import { handler as ssrHandler } from './dist/server/entry.mjs';
+import characters from './data/characters.json' with { type: 'json'};
 
 const app = express();
-
 const PORT = process.env.PORT ?? 1234;
 
+app.use(express.json());
+
 app.use('/', express.static('dist/client/'));
+app.get('./data/characters.json', (req, res) => {
+    res.json(characters)
+});
 
 app.use(ssrHandler);
 
